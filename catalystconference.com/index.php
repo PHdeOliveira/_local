b77999a0b593733a835afd227b9047cea8948ed8
Pedro Oliveira <pedro.hrde.oliveira@gmail.com>
2013-10-09 00:06:57 -0400

    attempts to bring twitter timeline

--- catalystconference.com/index.php
+++ catalystconference.com/index.php
@@ -15,9 +15,13 @@
 
 		<div class="container masonry">
 
-			<div id="jstwitter"></div>
-
 			<div id="sm" class="sm-twitter item">
+
+				<a class="twitter-timeline" href="https://twitter.com/twitterapi" data-widget-id="385848764910407680" width="280" height="156" data-chrome="noheader nofooter transparent noborders noscrollbar" data-tweet-limit="6">Tweets by @twitterapi</a>
+
+
+				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
 				
 				<p class="sm-date">Sept 16</p>
 
--- catalystconference.com/js/post-scripts.js
+++ catalystconference.com/js/post-scripts.js
@@ -12,11 +12,53 @@ var msnry = new Masonry( container, {
 });
 
 
-// Twitter Call
+function twitterCall() {
 
-$(function () {
-    // start jqtweet!
-    JQTWEET.loadTweets();
+ console.log('this is the twittercall');
 
-    console.log(JQTWEET);
-});
+ setTimeout(function(){
+
+	var frame = document.getElementById('twitter-widget-0');
+	var stream = frame.contentDocument.children[0].children[1].children[0].children[2];
+	var feed = stream.firstElementChild;
+	var feedChildren = feed.children;
+	var tweet;
+	var tweetBody;
+
+	for (var i = 0; i < feedChildren.length; i++) {
+		tweet = feedChildren[i];
+		tweetBody = tweet.children[2];
+		// tweetHeader = 
+
+		tweet.children[1].hidden = true;
+		tweet.children[3].hidden = true;
+
+		tweet.style.height = 156 + 'px';
+		tweet.style.position = 'relative';
+		tweet.style.boxSizing = 'border-box';
+		tweet.firstElementChild.style.position = 'absolute';
+		tweet.firstElementChild.style.right = 30 + 'px';
+		tweet.firstElementChild.style.bottom = 20 + 'px';
+
+		tweetBody.style.padding = '26px 30px 0';
+
+		console.dir(tweetBody);
+		console.log(tweet);
+
+
+		tweetBody.firstElementChild.style.color = '#fcfbf4';
+		tweetBody.firstElementChild.style.fontSize = 11 +'px';
+		tweetBody.firstElementChild.style.lineHeight = 13 + 'px';
+
+		
+		
+	}
+
+	// console.dir(feed);
+
+}, 500)
+
+}
+
+
+window.onload = twitterCall;
